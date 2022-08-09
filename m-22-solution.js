1246950b8a9fee1ea8133036ada6216507a1d465
meraj2085 <samirmeraj60@gmail.com>
2022-08-09 20:40:50 +0600

    adding m-22 solution 2

--- m-22-solution.js
+++ m-22-solution.js
@@ -14,3 +14,24 @@ const givenInput = areaOfTriangle(20, 30, 25);
 // প্রাকটিস চ্যালেঞ্জ-2:
 //-------------------------------------------------------------------------------\\
 
+function isPrime(number){
+     if(number <= 1){
+          return false;
+     }
+     else if(number === 2){
+          return true;
+     }
+     else if(number > 2){
+          for(i=2; i<number; i++){
+               if(number % i === 0){
+                    return false;
+               }
+               else{
+                    return true;
+               }
+          }
+     }
+}
+const number = 7;
+const result = isPrime(number);
+console.log(result);
